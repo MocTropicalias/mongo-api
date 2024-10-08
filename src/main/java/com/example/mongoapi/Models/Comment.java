f246366ddbd487ca0a35b324aecc2dff259e2fdc
nicolasruoco <nicolas.ruoco@germinare.org.br>
2024-10-08 11:11:02 -0300

    fix: solving a bug with swagger

--- src/main/java/com/example/mongoapi/Models/Comment.java
+++ src/main/java/com/example/mongoapi/Models/Comment.java
@@ -3,12 +3,8 @@ package com.example.mongoapi.Models;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public class Comment {
-    @Schema(description = "Id do usuário que comentou o post")
     private Long userId;
-    @Schema(description = "Url da imagem do usuário que comentou o post")
     private String userPhoto;
-    @Schema(description = "Nome do usuário que comentou o post")
     private String userName;
-    @Schema(description = "Conteúdo do comentário")
     private String content;
 }
