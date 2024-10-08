bf9a6d13c09d3b14804a99f8f6b45486eef5393f
nicolasruoco <nicolas.ruoco@germinare.org.br>
2024-10-08 11:09:15 -0300

    docs: creating the swagger documentation for the comment

--- src/main/java/com/example/mongoapi/Models/Comment.java
+++ src/main/java/com/example/mongoapi/Models/Comment.java
@@ -1,8 +1,14 @@
 package com.example.mongoapi.Models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class Comment {
+    @Schema(description = "Id do usuário que comentou o post")
     private Long userId;
+    @Schema(description = "Url da imagem do usuário que comentou o post")
     private String userPhoto;
+    @Schema(description = "Nome do usuário que comentou o post")
     private String userName;
+    @Schema(description = "Conteúdo do comentário")
     private String content;
 }
